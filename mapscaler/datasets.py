6c248bb0104dbb386bd5d58b27e23a91e0127844
Greg Condit <conditg@gmail.com>
2020-06-16 20:48:39 -0400

    Fixed Data Loader

--- mapscaler/datasets.py
+++ mapscaler/datasets.py
@@ -1,19 +1,22 @@
 import geopandas as gpd
-import json
 import os
+
 class MapLoader():
     
     def __init__(self):
-        self.path = 'geojson/'
-        self.USCB_path = 'https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html'
-    
+        self.path = os.path.dirname(os.path.realpath(__file__))
+        self.USCB_paths = {'population':
+          'https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/counties/totals/co-est2019-alldata.csv',
+          'geography':
+          'https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html'
+                         }
+        
     def fetch_counties(self, state_fips=None):
-        gdf = gpd.read_file(self.path + 'us_counties.json')
+        gdf = gpd.read_file(os.path.join(self.path,'geojson','us_counties.json'))
         if state_fips:
-            gdf = gdf[gdf.STATE==state_fips].copy()
-        return {'df':gdf, 'source':self.USCB_path}
+            gdf = gdf[gdf.STATE_FIPS==str(state_fips)].copy()
+        return {'df':gdf, 'sources':self.USCB_paths}
     
     def fetch_states(self):
-        print(self.path + 'us_states.json')
-        gdf = gpd.read_file(self.path + 'us_states.json')
-        return {'df':gdf, 'source':self.USCB_path}
+        gdf = gpd.read_file(os.path.join(self.path, 'geojson','us_states.json'))
+        return {'df':gdf, 'sources':self.USCB_paths}
